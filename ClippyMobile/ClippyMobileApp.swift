e644ad90909b57742c89f26054fabb8021783434
alansouls <maiaalan1111@gmail.com>
2026-01-10 19:55:56 +0000

    Initial Commit

---
+++ ClippyMobile/ClippyMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClippyMobileApp.swift
+//  ClippyMobile
+//
+//  Created by Alan Maia on 10/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClippyMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClippyMobile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClippyMobile
+//
+//  Created by Alan Maia on 10/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
